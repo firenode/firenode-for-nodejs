071deff47d33c496434351b8ef8575bb42e87a04
Christoph Dorn <christoph@christophdorn.com>
2015-05-15 09:55:55 -0700

    initial

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const CORE = require("org.firenode.core");
+// TODO: Use `require("engine.io")` once we can map main module in locator.
+const ENGINE_IO = require("engine.io/lib/engine.io");
+
+//console.log("CORE SERVER", CORE);
+//console.log("ENGINE_IO SERVER", ENGINE_IO);
+
+exports.main = function () {
+
+	console.log("firenode for server main!")
+
+
+var server = ENGINE_IO.listen(8089);
+
+server.on('connection', function(socket){
+console.log("NEW SOCKET");
+  socket.send('utf 8 string');
+  socket.send(new Buffer([0, 1, 2, 3, 4, 5])); // binary data
+});
+
+console.log("socket runing at", 8089);
+
+
+}
