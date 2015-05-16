3b2393a881579132c7694463f11f116be6bd8cde
Christoph Dorn <christoph@christophdorn.com>
2015-05-15 20:43:25 -0700

    wip

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const ENGINE_IO = require("engine.io/lib/engine.io");
 //console.log("CORE SERVER", CORE);
 //console.log("ENGINE_IO SERVER", ENGINE_IO);
 
-exports.main = function () {
+exports.main = function (options) {
 
 	console.log("firenode for server main!")
 
 
-var server = ENGINE_IO.listen(8089);
+var server = ENGINE_IO.listen(options.port);
 
 server.on('connection', function(socket){
 console.log("NEW SOCKET");
@@ -19,7 +19,7 @@ console.log("NEW SOCKET");
   socket.send(new Buffer([0, 1, 2, 3, 4, 5])); // binary data
 });
 
-console.log("socket runing at", 8089);
+console.log("socket runing at", options.port);
 
 
 }
